6aa024da893f716f18cec70a7220719d73bd23bf
Michael Steinmejer <mich86p0@edu.easj.dk>
2018-09-12 16:11:44 +0200

    første del lavet

---
+++ BilletLibrary/Bil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BilletLibrary
+{
+    /// <summary>
+    /// Dette er en public klasse bil
+    /// </summary>
+    public class Bil
+    {
+    /// <value>Dette er Nummerplade</value>
+        public string Nummerplade { get; set; }
+    /// <value>Dette er dato</value>
+        public DateTime Dato { get; set; }
+
+        /// <summary>
+        /// returnerer prisen
+        /// </summary>
+        /// <returns>240</returns>
+        public int Pris()
+        {
+            return 240;
+        }
+        /// <summary>
+        /// returnerer hvilket køretøj
+        /// </summary>
+        /// <returns>Bil</returns>
+        public string Køretøj()
+        {
+            return "Bil";
+        }
+    }
+}
